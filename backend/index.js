c2c27756cdbc2442351c71591c5884ed5e861fb2
Caio Fernandes <caio.fernandes.1216303@sga.pucminas.br>
2023-05-08 00:15:57 -0300

    Envia a porta para a Azure

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Azure Web App!');
+});
+
+const port = process.env.PORT || 80;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
