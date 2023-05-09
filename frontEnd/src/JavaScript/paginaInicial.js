a437d6bb1d23ba94919e328b71d5aa83208486e0
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-05-09 14:37:57 -0300

    Alterações nos cards + javascript

--- frontEnd/src/JavaScript/paginaInicial.js
+++ frontEnd/src/JavaScript/paginaInicial.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const thumbnails = document.querySelectorAll('#thumbnails img');
         
-    // adiciona um evento de click a cada imagem de thumb
+        // adiciona um evento de click a cada imagem de thumb
         thumbnails.forEach((thumbnail) => {
             thumbnail.addEventListener('click', () => {
                 // pega o indice da imagem clicada
@@ -16,3 +16,33 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
 });
+
+// Funçao para alterar a imagem dos cards ao passar o mouse
+function changeImage(card) {
+    var cardImage = card.querySelector('.card-image');
+        if (cardImage.src.includes('logotipo-do-xbox.png')){
+            cardImage.src = '/Geral/frontEnd/src/imgs/logotipo-do-xbox-black.png';
+        }
+        else if (cardImage.src.includes('logotipo-do-playstation.png')) {
+            cardImage.src = '/Geral/frontEnd/src/imgs/logotipo-do-playstation-black.png';
+        }
+        else if (cardImage.src.includes('pc.png')) {
+            cardImage.src = '/Geral/frontEnd/src/imgs/pc-black.png';
+        }
+}
+
+// Função para restaurar a imagem após retirar o mouse
+function restoreImage(card) {
+    var cardImage = card.querySelector('.card-image');
+    if (cardImage.src.includes('logotipo-do-xbox-black.png')){
+        cardImage.src = '/Geral/frontEnd/src/imgs/logotipo-do-xbox.png';
+    }
+    else if (cardImage.src.includes('logotipo-do-playstation-black.png')) {
+        cardImage.src = '/Geral/frontEnd/src/imgs/logotipo-do-playstation.png';
+    }
+    else if (cardImage.src.includes('pc-black.png')) {
+        cardImage.src = '/Geral/frontEnd/src/imgs/pc.png';
+    }
+  }
+
+
