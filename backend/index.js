43779488276bdca958db19aed722de5b5e5f4ea3
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2023-05-09 21:42:37 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Azure Web App!');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Teste');
+});
+
 app.listen(8080, () => {
   console.log('Servidor rodando na porta 80');
 });
