89bb066994aecbbfc7d7baca9fabd16c03d62922
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-05-17 22:17:55 -0300

    Removendo barra de rolagem

--- frontEnd/JavaScript/telaInfosJogo.js
+++ frontEnd/JavaScript/telaInfosJogo.js
@@ -1,22 +1,10 @@
 // Alterando a opacidade do site ao abrir o modal
 $('.openModal').click(function() {
     $('.modal').show();
-
-    // Corpo transparente
-    $('main').css({
-        'opacity': '50%'
-    });
-
-    // Cabeçalho transparente
-    $('header').css({
-        'opacity': '50%'
-    });
-
-    // Rodapé transparente
+    $('body').css('overflow', 'hidden');
   });
   
 // Fechando o modal ao clicar no x
 $('.close').click(function(){
     $('.modal').hide();
-    $('body').css('opacity', '100%');
 });
