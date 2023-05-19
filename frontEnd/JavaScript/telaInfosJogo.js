1bf63c096385048403885c3231f4ea9652fcfa9b
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-05-18 23:46:55 -0300

    Tentativa de consumir API

--- frontEnd/JavaScript/telaInfosJogo.js
+++ frontEnd/JavaScript/telaInfosJogo.js
@@ -5,6 +5,32 @@ $('.openModal').click(function() {
   });
   
 // Fechando o modal ao clicar no x
-$('.close').click(function(){
+$('.btn-close').click(function(){
     $('.modal').hide();
+    $('body').css('overflow-y', 'auto');
 });
+
+const id = '03o8at3jgk7vr2ob6iux3hrjqqya0g';
+const secret = 'j3ddf1zvjn1s8r5e98xw4pqfsavzx6';
+const token = 'ikxnw58wdz2ux847egryqas114flhx';
+
+const baseEndpoint = 'https://api.igdb.com/v4/games';
+
+// Configurar as opções da requisição
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    'Client-ID': id,
+    'Authorization': `Bearer ${token}`,
+  },
+};
+
+// Fazer a requisição à API
+fetch(baseEndpoint, requestOptions)
+  .then(response => response.json())
+  .then(gameData => {
+    console.log(gameData);
+  })
+  .catch(error => {
+    console.error('Erro na requisição:', error);
+  });
