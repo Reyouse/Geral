ae00a16e0da19f0b480d8eba1dc06161ac26007b
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-05-20 20:43:24 -0300

    Finalizando tela

--- frontEnd/JavaScript/telaInfosJogo.js
+++ frontEnd/JavaScript/telaInfosJogo.js
@@ -1,9 +1,23 @@
-// Alterando ícone do carrinho
-$(document).ready(function(){
-  $('#cartCheck').hide();
-
-  $('#cartAdd').click(function(){
+$(document).ready(function() {
+  $('#cartAdd').click(function() {
     $('#cartAdd').hide();
     $('#cartCheck').show();
+
+    setTimeout(function() {
+      $('#cartAdd').show();
+      $('#cartCheck').hide();
+    }, 3000); // 3 segundos (3000 milissegundos)
   });
 });
+
+function openFullscreen(image) {
+  if (image.requestFullscreen) {
+    image.requestFullscreen();
+  } else if (image.mozRequestFullScreen) { // Firefox
+    image.mozRequestFullScreen();
+  } else if (image.webkitRequestFullscreen) { // Chrome, Safari and Opera
+    image.webkitRequestFullscreen();
+  } else if (image.msRequestFullscreen) { // IE/Edge
+    image.msRequestFullscreen();
+  }
+}
