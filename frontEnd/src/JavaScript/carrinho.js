306d686723b72f72045c2025554ad0596d9a265b
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-05-20 12:56:38 -0300

    Botão salvar forma de pagamento + dropdown

--- frontEnd/src/JavaScript/carrinho.js
+++ frontEnd/src/JavaScript/carrinho.js
@@ -1,18 +1,3 @@
-var quantity = 2; // valor inicial
-
-  // botão de incremento
-  document.getElementById("incrementBtn").addEventListener("click", function() {
-    quantity++;
-    document.getElementById("quantityValue").innerText = quantity;
-  });
-
-  // botão de decremento
-  document.getElementById("decrementBtn").addEventListener("click", function() {
-    if (quantity > 0) {
-      quantity--;
-      document.getElementById("quantityValue").innerText = quantity;
-    }
-  });
 
 // botão de lixeira
 var trashButton = document.querySelector("#itemCarrinho .fa-trash-alt");
