0467055b9ce07ca9cb09a843cc79ef54df94fe2f
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-05-22 20:29:54 -0300

    Mudanças na parte de anúncios

--- frontEnd/src/JavaScript/infosUsuario.js
+++ frontEnd/src/JavaScript/infosUsuario.js
@@ -1,9 +1,9 @@
 function showContainer(containerId) {
-    const containers = document.getElementsByClassName('content-container');
-    for (let i = 0; i < containers.length; i++) {
-      containers[i].classList.remove('active');
-    }
-
-    const container = document.getElementById(containerId);
-    container.classList.add('active');
+  const containers = document.getElementsByClassName('content-container');
+  for (let i = 0; i < containers.length; i++) {
+    containers[i].classList.remove('active');
   }
+
+  const container = document.getElementById(containerId);
+  container.classList.add('active');
+}
