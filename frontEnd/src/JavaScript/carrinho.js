4d2c9ee5171ab05991d367cf360aae14b8868b9f
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2023-05-28 16:51:35 -0300

    Visualização do carrinho

--- frontEnd/src/JavaScript/carrinho.js
+++ frontEnd/src/JavaScript/carrinho.js
@@ -1,14 +1,6 @@
 
 // botão de lixeira
-var trashButton = document.querySelector("#itemCarrinho .fa-trash-alt");
 
-trashButton.addEventListener("click", function() {
-  // pega o item selecionado
-  var itemCarrinho = this.closest("#itemCarrinho");
-
-  // remove o item
-  itemCarrinho.remove();
-});
 
 
 
